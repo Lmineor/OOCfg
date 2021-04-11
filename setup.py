007666f07e05f7e079a160b42cfcb1d5011f77ec
lex <luohai2233@163.com>
2021-04-11 21:45:54 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@Author  : lex(luohai2233@163.com)
+"""
+import setuptools
+
+setuptools.setup(
+    setup_requires=['pbr>=2.0.0'],
+    pbr=True)
